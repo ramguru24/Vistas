0101b3b3e7ae81c5bc8d69d37170301936543f2b
ramguru24 <107878994+ramguru24@users.noreply.github.com>
2023-07-28 18:17:00 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,40 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>V-Learning Student App</title>
+</head>
+<body>
+    <section>
+        <div class="container mx-auto ">
+            <div class="row p-5 mx-auto">
+                Welcome!
+                <form class="mx-5">
+                    <label for="fname ">Name:</label><br>
+                    <input type="text" id="name" name="name" placeholder="Enter your Name" required><br>
+                    <label for="lname">Email:</label><br>
+                    <input type="mail" id="mail" name="mail" placeholder="Enter your Mail Id" required><br>
+                    <label for="email">Password:</label><br>
+                    <input type="text" id="password" name="password" placeholder="Enter your Password" required><br>
+                    <label for="email">Contact No:</label><br>
+                    <input type="text" id="contact" name="contact" placeholder="Enter your Contact No" required><br>
+                    <br>
+                    <input type="submit"   value="Submit">
+                  </form> 
+            </div>
+
+        </div>
+
+    </section>
+    
+</body>
+</html>
